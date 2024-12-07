3a019d9fc61de0a08f2bea0a485d6f9f2110be1a
Syed Rahbar Ali <rahbarali021@gmail.com>
2024-12-07 14:01:03 +0530

    "UI Library setup"

---
+++ src/components/layouts/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      {/* <Fooeter /> */}
+    </div>
+  );
+};
+
+export default Layout;
